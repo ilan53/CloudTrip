76c08263787671d945aea8f6239306276d69212f
litals1379 <litals1379@walla.com>
2025-06-06 15:24:41 +0300

    update

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   populateFromSelect();
   setupOrderButton();
+  const userEmail = localStorage.getItem("userEmail");
+  const myBookingsLink = document.getElementById("myBookingsLink");
+  if (userEmail && myBookingsLink) {
+    myBookingsLink.classList.remove("d-none");
+  }
 });
 
 async function populateFromSelect() {
